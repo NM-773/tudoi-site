06dfebbd88a4feb9ad19dcbdbcc7af9e4f87f35e
Nanami Maeda <nanami.github@gmail.com>
2021-07-31 15:15:36 +0900

    first commit

---
+++ Desktop/nanami/script.js
@@ -0,0 +1,10 @@
+$(function(){
+  $('header a').click(function(){
+    var id=$(this).attr('href');
+    var positon=$(id).offset().top;
+    $('html,body').animate({
+     "scrollTop":positon
+    },500);
+    });
+  
+  });
